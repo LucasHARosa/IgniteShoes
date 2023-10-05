df3e531620ebe23566d2a60e047036021bb2c851
LucasHARosa <l.ucas.henrique.a.r@gmail.com>
2023-10-05 16:56:15 -0300

    one signal

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-
+import {OneSignal} from 'react-native-onesignal';
 import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
@@ -9,6 +9,8 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
+
+OneSignal.initialize("4b35e650-0900-4fa1-989a-f974c4f93e52")
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
